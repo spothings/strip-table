4c0facad094322f66def7e7e642ffc40c815620d
Indra Wahyu <bukanspot@gmail.com>
2023-05-26 14:41:07 +0800

    connect to wifi

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,14 +1,27 @@
-int pin_ldr = A0;
-int pin_relay = 16;
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+
+int pin_ldr         = A0,
+    pin_relay       = 16
+;
+
+bool  relay_status  = false, 
+      gelap         = false
+;
 
 int relay_wait = 0;
-bool relay_status = false, gelap = false;
 
 void setup() {
   Serial.begin(115200);
   pinMode(pin_ldr, INPUT);
   pinMode(pin_relay, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+
+  WifiSetup();
 }
 
 void loop() {
---
+++ src/main/wifi.ino
@@ -0,0 +1,12 @@
+void WifiSetup(){
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
