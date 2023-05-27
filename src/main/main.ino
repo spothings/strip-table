2b442ceae683efb8920828fc5c8ee773aa069696
Indra Wahyu <bukanspot@gmail.com>
2023-05-27 20:59:34 +0800

    add comment for colaboretion

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,19 +2,20 @@
 #include <ESP8266WiFi.h>
 
 const int
-  pin_ldr = A0,
-  pin_relay = 16,
-  pin_led = LED_BUILTIN,
-  relay_delay = 60;
+  pin_ldr = A0,           // pin LDR
+  pin_relay = 16,         // pin relay
+  pin_led = LED_BUILTIN,  // pin LED
+  relay_delay = 60;       // relay delay to turn on or off relay
 
 int
-  RELAYWAIT = 0,
-  MAXLDR = 0,
-  MINLDR = 1024;
+  RELAYWAIT = 0,  // increment value to wait condision
+  MAXLDR = 0,     // min value for LDR by default
+  MINLDR = 1024;  // max value for LDR by default
 
 bool
-  RELAYSTATUS = false,
-  GELAP = false;
+  RELAYSTATUS = true,  // relay status on or off
+  GELAP = true,        // light status (gelap or terang)
+  MALAM = true;
 
 void setup() {
   Serial.begin(115200);
@@ -22,27 +23,45 @@ void setup() {
   pinMode(pin_relay, OUTPUT);
   pinMode(pin_led, OUTPUT);
 
-  Relay(pin_relay, false);
+  // Relay(pin_relay, false);  // by default turn off relay
   WifiSetup();
 }
 
 void loop() {
+  // get LDR value
   int intensitas = LdrAverage(pin_ldr);
+
+  // set max and min LDR value with average
   int nilaigelap = IntensitasAverage(intensitas, MAXLDR, MINLDR);
 
-  if (intensitas < nilaigelap) {
-    if (GELAP == false){
-      RELAYWAIT = 0;
-      GELAP = true;
+  // set light status (gelap or terang)
+  if (MALAM) {
+    if (intensitas < nilaigelap) {
+      if (GELAP == false) {
+        RELAYWAIT = 0;
+        GELAP = true;
+      }
+    } else {
+      if (GELAP == true) {
+        RELAYWAIT = 0;
+        GELAP = false;
+      }
     }
   } else {
-    if (GELAP == true){
-      RELAYWAIT = 0;
-      GELAP = false;
-    }
+    GELAP = false;
+    RELAYSTATUS = false;
   }
 
+  // CODE FOR NTP CLIENT
+  // amen peteng, set variabel -> MALAM = true | amen tengai, set variablel -> MALAM = false
+
+
+  // turn on built in led by light status
   Leds(pin_led, GELAP);
+
+  // turn on or off relay
   RelayStatus(pin_relay, RELAYSTATUS, GELAP, relay_delay);
+
+  // print to serial monitor
   SerialMonitor(intensitas, RELAYWAIT, MAXLDR, MINLDR, nilaigelap, GELAP);
 }
