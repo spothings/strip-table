6b40d17c2680c7824bc15b9874dae9cf98721244
Indra Wahyu <bukanspot@gmail.com>
2023-06-01 04:02:34 +0800

    implement telegram bot

--- src/main/built-in-led.ino
+++ src/main/built-in-led.ino
@@ -1,5 +1,5 @@
 void Leds(int _pin, bool _value){
-  if(!_value){
+  if(_value){
     digitalWrite(_pin, HIGH);
   } else {
     digitalWrite(_pin, LOW);
---
+++ src/main/telegram.ino
@@ -0,0 +1,75 @@
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+
+// Checks for new messages every 1 second.
+unsigned long lastTimeBotRan;
+
+// Handle what happens when you receive new messages
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+
+  for (int i = 0; i < numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID) {
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+    }
+
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println(text);
+
+    String from_name = bot.messages[i].from_name;
+
+    if (text == "/status") {
+      if (SLEEP) {
+        bot.sendMessage(chat_id, "system is sleep", "");
+      } else {
+        bot.sendMessage(chat_id, "system is wakeup", "");
+      }
+    }
+
+    if (text == "/sleep") {
+      Relay(pin_relay, false);
+      SLEEP = true;
+      TDELAY = 0;
+      if (SLEEP) {
+        bot.sendMessage(chat_id, "system is sleep", "");
+      } else {
+        bot.sendMessage(chat_id, "system is wakeup", "");
+      }
+    }
+
+    if (text == "/wakeup") {
+      Relay(pin_relay, true);
+      SLEEP = false;
+      TDELAY = 10000;
+      if (SLEEP) {
+        bot.sendMessage(chat_id, "system is sleep", "");
+      } else {
+        bot.sendMessage(chat_id, "system is wakeup", "");
+      }
+    }
+  }
+}
+
+void TelegramSetup() {
+  configTime(0, 0, "pool.ntp.org");  // get UTC time via NTP
+  client.setTrustAnchors(&cert);     // Add root certificate for api.telegram.org
+}
+
+void Telegram() {
+  if (millis() > lastTimeBotRan + TDELAY) {
+    Serial.println("read telegram message");
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+    while (numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
+}
